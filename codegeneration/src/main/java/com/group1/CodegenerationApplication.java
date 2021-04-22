b17e330c1cd5ab7a2c5dc826d1bf145c213d6dbc
ADijk <ashley_dijkshoorn@hotmail.com>
2021-04-22 12:54:32 +0200

    Framework spring boot
    Framework spring boot

---
+++ codegeneration/src/main/java/com/group1/CodegenerationApplication.java
@@ -0,0 +1,13 @@
+package com.group1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodegenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodegenerationApplication.class, args);
+	}
+
+}
