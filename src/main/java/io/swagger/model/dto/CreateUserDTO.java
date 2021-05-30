e750c30e7787a7981a79011bffb4224b7d78afd1
Samuel-code-design <54932034+Samuel-code-design@users.noreply.github.com>
2021-05-30 19:07:13 +0200

    laatste wijziggingen voor merge

--- src/main/java/io/swagger/model/dto/CreateUserDTO.java
+++ src/main/java/io/swagger/model/dto/CreateUserDTO.java
@@ -9,7 +9,6 @@ public class CreateUserDTO {
 
     private String username;
     private String password;
-    private String password2;
     private String firstName;
     private String lastName;
     private String email;
@@ -24,10 +23,9 @@ public class CreateUserDTO {
         this.roles = role;
     }
 
-    public CreateUserDTO(String username, String password, String password2, String firstName, String lastName, String email, String phone, List<Role> roles) {
+    public CreateUserDTO(String username, String password, String firstName, String lastName, String email, String phone, List<Role> roles) {
         this.username = username;
         this.password = password;
-        this.password2 = password2;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -49,14 +47,6 @@ public class CreateUserDTO {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getPassword2() {
-        return password2;
-    }
-
-    public void setPassword2(String password2) {
-        this.password2 = password2;
     }
 
     public String getFirstName() {
--- src/main/java/io/swagger/service/EmployeeService.java
+++ src/main/java/io/swagger/service/EmployeeService.java
@@ -2,48 +2,97 @@ package io.swagger.service;
 
 import io.swagger.model.dto.CreateUserDTO;
 import io.swagger.model.User;
+import io.swagger.models.auth.In;
 import io.swagger.repository.EmployeeRepository;
+import org.apache.tomcat.util.bcel.Const;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.math.BigDecimal;
 import java.util.List;
+
 
 @Service
 public class EmployeeService {
-    EmployeeRepository repository;
+    private final EmployeeRepository repository;
 
     @Autowired
     public EmployeeService(EmployeeRepository repository) {
         this.repository = repository;
     }
 
-    public void createUser(CreateUserDTO u){
-        //TODO: const maken voor daylimit en trasnlimit
-        //bestaan gegevens al
-        //passwrod lang genoeg??
+    public void createUser(CreateUserDTO newUser){
+        Long DAY_LIMIT = 1000L;
+        Long TRANSACTION_LIMIT = 1000L;
 
+//        int MINIMUM_PASSWORD_LENGTH = 10;
+//        String password = newUser.getPassword();
+//        String email = newUser.getEmail();
+//        String username = newUser.getUsername();
+//        String phone = newUser.getPhone();
+//
+//        boolean userExists = false;
+//        boolean passTooShort = false;
+//        boolean usernameInUse = false;
+//
+//        for (User u: repository.findAll()) {
+//            if (u.getEmail().equals(email) ||
+//                    u.getPhone().equals(phone)){
+//                userExists = true;
+//                break;
+//            }
+//            else if (password.length() < MINIMUM_PASSWORD_LENGTH){
+//                passTooShort = true;
+//                break;
+//            }
+//            else if (u.getUsername().equals(username)){
+//                usernameInUse = true;
+//                break;
+//            }
+//        }
+//
+//        if (userExists){
+//            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "User already exists");
+//        }
+//        else if (passTooShort){
+//            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Password is too short, choose a longer one and try again");
+//        }else if(usernameInUse){
+//            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Username is already in use, please try again and choose a different one");
+//        }
+//        else{
+//            User user = new User();
+//            user.setId(null);
+//            user.setLocked(false);
+//            user.setDayLimit(DAY_LIMIT);
+//            user.setTransactionLimit(TRANSACTION_LIMIT);
+//
+//            user.setEmail(email);
+//            user.setPassword(password);
+//            user.setUsername(username);
+//            user.setFirstName(newUser.getFirstName());
+//            user.setLastName(newUser.getLastName());
+//            user.setPhone(newUser.getPhone());
+//            user.setRoles(newUser.getRole());
+//
+//            repository.save(user);
+//        }
         User user = new User();
-        user.setId(null);
-        user.setLocked(false);
-        user.setDayLimit(1000L);
-        user.setEmail(u.getEmail());
-        user.setUsername(u.getUsername());
-        user.setFirstName(u.getFirstName());
-        user.setLastName(u.getLastName());
-        user.setPassword(u.getPassword());
-        user.setPhone(u.getPhone());
-        user.setRoles(u.getRole());
-        user.setTransactionLimit(1000L);
+            user.setId(null);
+            user.setLocked(false);
+            user.setDayLimit(DAY_LIMIT);
+            user.setTransactionLimit(TRANSACTION_LIMIT);
 
-        repository.save(user);
+            user.setEmail(newUser.getEmail());
+            user.setPassword(newUser.getPassword());
+            user.setUsername(newUser.getUsername());
+            user.setFirstName(newUser.getFirstName());
+            user.setLastName(newUser.getLastName());
+            user.setPhone(newUser.getPhone());
+            user.setRoles(newUser.getRole());
 
-        if (true) {
-
-        } else {
-            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Username is already in use, , please try again and choose a different one");
-        }
+            repository.save(user);
     }
 
     public void lockUserByEmail(String email) {
@@ -59,13 +108,12 @@ public class EmployeeService {
     }
 
     public void updateUser(User body) {
-        //TODO: bestaat new username al
         repository.save(body);
     }
 
     public List<User> getUsers(String searchString) {
         if (searchString != null){
-            return repository.findAllByEmailContaining(searchString);
+            return repository.findAllByEmailContaining(searchString); //TODO: ook username fistname en lastname
         }
         return repository.findAll();
     }
@@ -76,7 +124,6 @@ public class EmployeeService {
 
     public User userById(Long id) {
         return repository.findByIdEquals(id);
-//        return repository.getOne(id);
     }
 
 }
